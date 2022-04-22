818c8e0f4ca269dbcd3b343d1106e982c51ce765
Adroaldo <hygino@msn.com>
2022-04-22 08:27:57 -0300

    Implementado a soma das matrizes

--- matriz.py
+++ matriz.py
@@ -1,3 +1,17 @@
+def abrirMatriz(arq: str):
+    arquivo = open(arq, "r")
+    matriz = []
+
+    linha = arquivo.readline()
+    while(linha != ""):
+        elementos = linha.split(' ')
+        for n in range(len(elementos)):
+            elementos[n] = float(elementos[n])
+        matriz.append(elementos)
+    arquivo.close()
+    return matriz
+
+
 def entradaMatriz():
     l = int(input('Informe o número de linhas'))
     c = int(input('Informe o número de colunas'))
@@ -7,7 +21,7 @@ def entradaMatriz():
         linha = []
         for j in range(c):
             valor = float(
-                input(f'Informe o elemento da linha {i} e coluna {j}'))
+                input(f'Informe o elemento da linha {i} e coluna {j} -> '))
             linha.append(valor)
         matriz.append(linha)
 
@@ -46,18 +60,36 @@ def matrizSoma(ma, mb):
     cb = len(ma[0])  # indica a quantidade de colunas de B
     if(la != lb and ca != cb):
         print('Não é possivel somar as matrizes,\npois suas dimensões são incompatíveis.')
+        return
     else:
         print('Podemos somar as matrizes.')
+        matriz = []
+        for i in range(la):
+            linha = []
+            for j in range(cb):
+                elemento = ma[i][j]+mb[i][j]
+                linha.append(elemento)
+            matriz.append(linha)
+        return matriz
 
 
 a = [[2, 3, 4],
      [5, 6, 7]]
 
-b = matrizEscalar(3, a)
+b = [[-1, 3.5, 9],
+     [8, 2, 3]]
+
+print('Matriz A')
+imprime(a)
 print('Matriz B')
 imprime(b)
+c = matrizSoma(a, b)
+print('Matriz soma')
+imprime(c)
+# print('Matriz B')
+# imprime(b)
 
-matrizSoma(a, b)  # Não implementado completamente ainda
-t = [[1, 2, 3],
-     [4, 5, 6],
-     [7, 8, 2]]
+# matrizSoma(a, b)  # Não implementado completamente ainda
+# t = [[1, 2, 3],
+#      [4, 5, 6],
+#      [7, 8, 2]]
